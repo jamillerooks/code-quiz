93cb77366c6630ccf6ee4a9edfb9da7e882a49cd
Jamille Rooks <jamillerooks@outlook.com>
2022-01-18 10:51:29 -0500

    Create array of questions and answers in JavaScript.

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 //Create the quiz data
+var quizInput = [
 
+    question:   "Commonly used data types DO NOT include:",
+    1.  "strings",
+    2.  "booleans",
+    3.  "alerts",
+    4.  "numbers",
+    correct: "ans3",
+    //ToDo write an Alert to say Correct or Wrong
+
+    question:   "The condition in an if / else statement is enclosed within _____.",
+    1.  "quotes",
+    2.  "curly brackets",
+    3.  "parenthesis", 
+    4.  "square brackets",
+    correct: "ans3",
+
+    question:   "Arrays in JavaScript can be used to store _____.",
+    1.  "numbers and strings",
+    2.  "other arrays",
+    3.  "booleans",
+    4.  "all of the above",
+    correct: "ans4",
+    
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    1.  "commas",
+    2.  "curly brackets",
+    3.  "quotes",
+    4.  "parenthesis",
+    correct: "ans3",
+
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    1.  "JavaScript",
+    2.  "terminal/bash",
+    3.  "for loops",
+    4.  "console.log",
+    correct: "ans4",
+
+    
+]
 
 
 
